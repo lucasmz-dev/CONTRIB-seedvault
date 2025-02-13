e41405d6f06676a69588d6ba53ff2cc661d3f7ff
Torsten Grote <t@grobox.de>
2025-02-13 14:44:38 -0300

    Also abort file backup while backup up a single huge file
    previously switching to a metered network would have left the upload to continue until the file would be complete

--- storage/lib/src/main/java/org/calyxos/backup/storage/backup/FileBackup.kt
+++ storage/lib/src/main/java/org/calyxos/backup/storage/backup/FileBackup.kt
@@ -44,7 +44,7 @@ internal class FileBackup(
         files.forEach { file ->
             if (wasAborted()) throw IOException("Metered Network")
             val result = try {
-                backupFile(file, availableChunkIds)
+                backupFile(file, availableChunkIds, wasAborted)
             } catch (e: IOException) {
                 backupObserver?.onFileBackupError(file, "L")
                 Log.e(TAG, "Error backing up ${file.uri}", e)
@@ -81,6 +81,7 @@ internal class FileBackup(
     private suspend fun backupFile(
         file: ContentFile,
         availableChunkIds: Set<String>,
+        wasAborted: () -> Boolean,
     ): FileBackupResult {
         val cachedFile = filesCache.getByUri(file.uri)
         val missingChunkIds = cachedFile?.chunks?.minus(availableChunkIds) ?: emptyList()
@@ -94,7 +95,7 @@ internal class FileBackup(
             chunker.makeChunks(inputStream)
         }
         val chunkWriterResult = uri.openInputStream(contentResolver).use { inputStream ->
-            chunkWriter.writeChunk(inputStream, chunks, missingChunkIds)
+            chunkWriter.writeChunk(inputStream, chunks, missingChunkIds, wasAborted)
         }
 
         val chunkIds = chunks.map { it.id }
